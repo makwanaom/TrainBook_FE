79ad978f38b8d14dc7673424c82d8863d3f39bb5
makwanaom <117265350+makwanaom@users.noreply.github.com>
2025-01-02 19:35:47 +0530

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">Train Booking</h1>
+        <div>
+          <Link to="/" className="px-4 py-2 hover:underline">Home</Link>
+          <Link to="/signup" className="px-4 py-2 hover:underline">Signup</Link>
+          <Link to="/login" className="px-4 py-2 hover:underline">Login</Link>
+          <Link to="/seats" className="px-4 py-2 hover:underline">Seat Booking</Link>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
